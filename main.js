f681f18163f5143437c396c2f648c494f67a9146
yoyo <yoyointheho@gmail.com>
2021-09-15 12:37:50 +0900

    learned v-if

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const app = Vue.createApp({
 	data() {
 		return {
+			showBooks: true,
 			title: 'yoyo and the ho',
 			author: 'yoyo',
 			age: 12,
 		};
+	},
+
+	methods: {
+		// changeTitle() {
+		// 	this.title = 'Words of Randiance';
+		// },
+		toggleShowBooks() {
+			this.showBooks = !this.showBooks;
+		},
 	},
 });
 
