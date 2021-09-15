908543955f3c189953ce804557a91af37b6eb233
yoyo <yoyointheho@gmail.com>
2021-09-16 08:50:57 +0900

    learned v-for

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ const app = Vue.createApp({
 	data() {
 		return {
 			showBooks: true,
-			title: 'yoyo and the ho',
-			author: 'yoyo',
-			age: 12,
+
+			books: [
+				{ title: 'yoyo', author: 'nathan', age: 20 },
+				{ title: 'mobydick', author: 'jonathan', age: 30 },
+				{ title: 'bird is flying', author: 'nathan', age: 50 },
+			],
+			x: 0,
+			y: 0,
 		};
 	},
 
@@ -14,6 +19,16 @@ const app = Vue.createApp({
 		// },
 		toggleShowBooks() {
 			this.showBooks = !this.showBooks;
+		},
+		handleEvent(e, data) {
+			console.log(e, e.type);
+			if (data) {
+				console.log(data);
+			}
+		},
+		handleMousemove(e) {
+			this.x = e.offsetX;
+			this.y = e.offsetY;
 		},
 	},
 });
