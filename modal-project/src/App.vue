9850e3c137cd66834d366803a08b4cbbb55a8ba1
yoyo <yoyointheho@gmail.com>
2021-09-16 16:38:01 +0900

    learned CSS scope and props

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,12 +1,28 @@
 <template>
 	<h1>{{ title }}</h1>
+	<Modal :customProp="customProp" :text="text" theme="sale" />
 </template>
 
 <script>
+import Modal from './components/Modal.vue';
+
 export default {
 	name: 'App',
+	components: { Modal },
 	data() {
-		return { title: 'my first Vue app' };
+		return {
+			title: 'my first Vue app',
+			customProp: 'this is customProp',
+			text: 'this is text',
+		};
+	},
+	methods: {
+		handleClick() {
+			//querySelector of Vue
+			console.log(this.$refs.name);
+			this.$refs.name.classList.add('active');
+			this.$refs.name.focus();
+		},
 	},
 };
 </script>
@@ -19,5 +35,8 @@ export default {
 	text-align: center;
 	color: #2c3e50;
 	margin-top: 60px;
+}
+h1 {
+	font-family: 'Times New Roman', Times, serif;
 }
 </style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="backdrop">
+		<div class="modal" :class="{ sale: theme === 'sale' }">
+			<h1>{{ customProp }}</h1>
+			<p>{{ text }}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['customProp', 'text', 'theme'],
+};
+</script>
+
+<style scoped>
+/* scoped using data-attribute to scope css. .h1[data-4145151] */
+.modal {
+	width: 400px;
+	padding: 20px;
+	margin: 100px auto;
+	background: white;
+	border-radius: 10px;
+}
+
+.backdrop {
+	top: 0;
+	position: fixed;
+	background: rgba(0, 0, 0, 0.5);
+	width: 100%;
+	height: 100%;
+}
+
+h1 {
+	color: turquoise;
+	text-shadow: tomato 10px 10px;
+}
+
+.sale {
+	background: crimson;
+	color: white;
+}
+
+.sale h1 {
+	color: white;
+}
+</style>
