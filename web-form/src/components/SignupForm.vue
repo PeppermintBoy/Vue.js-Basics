9add0af51b9790e1a0083d48f711402fe9082594
yoyo <yoyointheho@gmail.com>
2021-09-20 12:53:34 +0900

    Learned keyboard key modifiers. @keyup.alt

---
+++ web-form/src/components/SignupForm.vue
@@ -0,0 +1,110 @@
+<template>
+	<form>
+		<label>Email:</label>
+		<input type="email" required v-model="email" />
+
+		<label>Password:</label>
+		<input type="password" required v-model="password" />
+
+		<label>Role:</label>
+		<select v-model="role">
+			<option value="---">- - -</option>
+			<option value="developer">web Developer</option>
+			<option value="designer">web Designer</option>
+		</select>
+
+		<label>Skills</label>
+		<input type="text" v-model="tempSkill" @keyup="addSkill" />
+		<div v-for="skill in skills" :key="skill" class="pill">
+			{{ skill }}
+		</div>
+
+		<div class="terms">
+			<input type="checkbox" v-model="terms" required />
+			<label>Accept terms and conditions</label>
+		</div>
+
+		<div>
+			<input type="checkbox" value="Nathan" v-model="names" />
+			<label>Nathan</label>
+		</div>
+		<div>
+			<input type="checkbox" value="Yoyo" v-model="names" />
+			<label>Yoyo</label>
+		</div>
+		<div>
+			<input type="checkbox" value="Shosuke" v-model="names" />
+			<label>Shosuke</label>
+		</div>
+	</form>
+	<p>email: {{ email }}</p>
+	<p>password: {{ password }}</p>
+	<p>role: {{ role }}</p>
+	<p>Terms accepted: {{ terms }}</p>
+	<p>Names: {{ names }}</p>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			email: '',
+			password: '',
+			role: '---',
+			terms: false, //when checkbox is clikced, toggles
+			names: [],
+			tempSkill: '',
+			skills: [],
+		};
+	},
+	methods: {
+		addSkill(e) {
+			//fires when "," pressed and textbox is not empty
+			if (e.key === 'Enter' && this.tempSkill) {
+				if (!this.skills.includes(this.tempSkill)) {
+					this.skills.push(this.tempSkill);
+				}
+				this.tempSkill = '';
+			}
+		},
+	},
+};
+</script>
+
+<style>
+form {
+	max-width: 420px;
+	margin: 30px auto;
+	background: white;
+	text-align: left;
+	padding: 40px;
+	border-radius: 10px;
+}
+label {
+	color: #aaa;
+	display: inline-block;
+	margin: 25px 0 15px;
+	font-size: 0.6em;
+	text-transform: uppercase;
+	letter-spacing: 1px;
+	font-weight: bold;
+}
+input,
+select {
+	display: block;
+	padding: 10px 6px;
+	width: 100%;
+	box-sizing: border-box;
+	border: none;
+	border-bottom: 1px solid #ddd;
+	color: #555;
+}
+
+input[type='checkbox'] {
+	display: inline-block;
+	width: 16px;
+	margin: 0 10px 0 0;
+	position: relative;
+	top: 2px;
+}
+</style>
