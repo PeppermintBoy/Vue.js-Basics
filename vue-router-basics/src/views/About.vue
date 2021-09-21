b4feae9b9154762b49bc61884c490239ca833b90
yoyo <yoyointheho@gmail.com>
2021-09-21 12:18:42 +0900

    Learned router links

--- vue-router-basics/src/views/About.vue
+++ vue-router-basics/src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
 	<div class="about">
-		<h1>This is an about page or is it</h1>
+		<h1>About</h1>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro accusamus
+			adipisci minima facere a consequatur earum architecto numquam qui libero.
+		</p>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro accusamus
+			adipisci minima facere a consequatur earum architecto numquam qui libero.
+		</p>
 	</div>
 </template>
--- vue-router-basics/src/views/Home.vue
+++ vue-router-basics/src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
 	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<h1>Homepage</h1>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, est alias.
+		Cupiditate, earum? Sunt fugit voluptatibus aperiam ea earum. Quasi voluptas
+		dolor officia eligendi voluptate suscipit sapiente ea laborum excepturi
+		atque, dicta quod odio doloremque iusto sequi. Consequuntur impedit sunt id
+		delectus voluptates atque? Voluptatem consequatur error quia architecto
+		cumque delectus similique accusantium, ex quos, illo dignissimos impedit
+		provident officia odit dolor quibusdam magni quod porro consequuntur
+		molestiae perspiciatis? Ullam eos sunt eius, repellat enim, asperiores
+		temporibus provident veritatis recusandae qui quidem facere magni
+		repudiandae sapiente veniam inventore, ipsum nihil! Excepturi cum minus
+		aperiam harum omnis inventore deserunt dolore ut.
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, est alias.
+			Cupiditate, earum? Sunt fugit voluptatibus aperiam ea earum. Quasi
+			voluptas dolor officia eligendi voluptate suscipit sapiente ea laborum
+			excepturi atque, dicta quod odio doloremque iusto sequi. Consequuntur
+			impedit sunt id delectus voluptates atque? Voluptatem consequatur error
+			quia architecto cumque delectus similique accusantium, ex quos, illo
+			dignissimos impedit provident officia odit dolor quibusdam magni quod
+			porro consequuntur molestiae perspiciatis? Ullam eos sunt eius, repellat
+			enim, asperiores temporibus provident veritatis recusandae qui quidem
+			facere magni repudiandae sapiente veniam inventore, ipsum nihil! Excepturi
+			cum minus aperiam harum omnis inventore deserunt dolore ut.
+		</p>
 	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
 	name: 'Home',
-	components: {
-		HelloWorld,
-	},
+	components: {},
 };
 </script>
