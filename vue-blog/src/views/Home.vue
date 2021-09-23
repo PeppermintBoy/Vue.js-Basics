f1bfedafd1f481b2fe7e6a2be6f318f8f6715d78
yoyo <yoyointheho@gmail.com>
2021-09-23 15:45:47 +0900

    Learned composition API basics

---
+++ vue-blog/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="home">
+		Home
+		<p>My name is {{ name }} and my age is {{ age }}</p>
+		<button @click="handleClick">click me</button>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+	name: 'Home',
+	//setup excutes first. even earlier than created()
+	setup() {
+		console.log('setup');
+
+		let name = 'mario';
+		let age = 30;
+
+		const handleClick = () => {
+			console.log('you clicked me');
+		};
+
+		return { name, age, handleClick };
+	},
+};
+</script>
--- vue-router-basics/src/views/Jobs/JobDetails.vue
+++ vue-router-basics/src/views/Jobs/JobDetails.vue
@@ -1,8 +1,13 @@
 <template>
-	<h1>{{ job.title }}</h1>
-	<!-- calls the parameter of current route. (doesnt need to be "id") -->
-	<p>The job id is {{ id }}</p>
-	<p>{{ job.details }}</p>
+	<div v-if="job">
+		<h1>{{ job.title }}</h1>
+		<!-- calls the parameter of current route. (doesnt need to be "id") -->
+		<p>The job id is {{ id }}</p>
+		<p>{{ job.details }}</p>
+	</div>
+	<div v-else>
+		<p>Loading job details ...</p>
+	</div>
 </template>
 
 <script>
@@ -14,7 +19,7 @@ export default {
 		};
 	},
 	mounted() {
-		fetch('http://localhost:3000/jobs' + this.id)
+		fetch('http://localhost:3000/jobs/' + this.id)
 			.then(res => res.json())
 			.then(data => (this.job = data))
 			.catch(err => cosole.log(err.message));
