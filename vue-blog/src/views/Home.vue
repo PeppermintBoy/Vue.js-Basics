2a659af226407dece71f416d772eaac96cbc8036
yoyo <yoyointheho@gmail.com>
2021-09-25 09:02:47 +0900

    Learned ref vs reactive. ref needs .value but will work with primitive. Ref is the choice for most situations

--- vue-blog/src/views/Home.vue
+++ vue-blog/src/views/Home.vue
@@ -1,16 +1,23 @@
 <template>
 	<div class="home">
-		Home
+		<h1>home</h1>
 		<!-- No need to name.value or age.value for output -->
-		<p>My name is {{ name }} and my age is {{ age }}</p>
+		<!-- <p>My name is {{ name }} and my age is {{ age }}</p>
 		<button @click="handleClick">change name to luigi</button>
 		<button @click="age++">add 1 to age</button>
-		<input type="text" v-model="name" />
+		<input type="text" v-model="name" /> -->
+		<h2>refs</h2>
+		<p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+		<button @click="updateNinjaOne">update ninja one</button>
+
+		<h2>reactive</h2>
+		<p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+		<button @click="updateNinjaTwo">update ninja two</button>
 	</div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity';
+import { ref, reactive } from '@vue/reactivity';
 // @ is an alias to /src
 
 export default {
@@ -19,19 +26,28 @@ export default {
 	//with setup, 'this' does not work. returns undefined
 	//$refs will not work without this, so use ref(). automatically imports from vue
 	setup() {
-		console.log('setup');
+		// console.log('setup');
 
-		// const para = ref(null);
+		// // const para = ref(null);
 
-		//when you wrap variable with ref(), it becomes reactive
-		const name = ref('mario');
-		const age = ref(30);
+		// //when you wrap variable with ref(), it becomes reactive
+		// const name = ref('mario');
+		// const age = ref(30);
 
-		const handleClick = () => {
-			name.value = 'luigi';
+		// const handleClick = () => {
+		// 	name.value = 'luigi';
+		// };
+
+		const ninjaOne = ref({ name: 'mario', age: 30 });
+		const ninjaTwo = reactive({ name: 'luigi', age: 20 });
+
+		const updateNinjaOne = () => {
+			ninjaOne.value.age = 40;
 		};
-
-		return { name, age, handleClick };
+		const updateNinjaTwo = () => {
+			ninjaTwo.age = 50;
+		};
+		return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo };
 	},
 };
 </script>
